9cc3e111c352024b7ccd73a8cb6b33fd5d87467c
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-08-09 18:06:59 -0600

    [example] Updated example

--- main.c
+++ main.c
@@ -9,29 +9,54 @@
 // pack module
 #include <pack/pack.h>
 
+typedef struct
+{
+    float x, y, z;
+} vec3;
+
+typedef struct
+{
+    vec3 loc, rot, sca;
+} transform;
+
+int transform_pack ( transform _transform, char *p_buffer );
+
 // Entry point
 int main ( int argc, const char *argv[] )
 {
 
     // Initialized data
-    char _buffer[512] = { 0 };
+    char _buf[512] = { 0 };
     FILE *p_f = fopen("../output.bin", "w+");
-    size_t written = 0;
+    size_t i = 0;
+    transform _transform = 
+    {
+        .loc = { .x = 4.f , .y =   3.f, .z = 5.f },
+        .rot = { .x = 90.f, .y = -30.f, .z = -45.f },
+        .sca = { .x = 1.f , .y =   1.f, .z = 1.f }
+    };
     
     // Pack some data
-    written = pack_pack(
-        &_buffer,
-        "%3i8 %i16 %i32 %i64 %2f32 %2f64",
-        'a', 'b', 'c',
-        31415,
-        2718281,
-        1618033988749,
-        3.1415927f, 6.2831855f,
-        3.141592653589793, 6.283185307179586
-    );
+    i += pack_pack(&_buf[i], "%3s"           , "Hello, World!", "Hi Mom!", "Hi Dad");
+    i += pack_pack(&_buf[i], "%3i8"          , 'a', 'b', 'c');
+    i += pack_pack(&_buf[i], "%i16 %i32 %i64", 31415, 2718281, 1618033988749);
+    i += pack_pack(&_buf[i], "%2f32"         , 3.1415927f, 6.2831855f);
+    i += pack_pack(&_buf[i], "%2f64"         , 3.141592653589793, 6.283185307179586);
+    
+    // Write the binary data to the output file
+    fwrite(&_buf, 1, i, p_f);
+
+    // Close the file
+    fclose(p_f);
+
+    // Reset
+    i = 0, p_f = fopen("../transform.bin", "w+");
+
+    // Pack a struct using a function
+    i = transform_pack(_transform, &_buf);
 
     // Write the binary data to the output file
-    fwrite(&_buffer, 1, written, p_f);
+    fwrite(&_buf, 1, i, p_f);
 
     // Close the file
     fclose(p_f);
@@ -40,3 +65,24 @@ int main ( int argc, const char *argv[] )
     return EXIT_SUCCESS;
 }
 
+int vec3_pack ( vec3 _vec3, char *p_buffer )
+{
+    
+    // Done
+    return pack_pack(p_buffer, "%3f32", _vec3.x, _vec3.y, _vec3.z);
+}
+
+int transform_pack ( transform _transform, char *p_buffer )
+{
+    
+    // Initialized data
+    size_t i = 0;
+
+    // Pack some data
+    i += vec3_pack(_transform.loc, &p_buffer[i]);
+    i += vec3_pack(_transform.rot, &p_buffer[i]);
+    i += vec3_pack(_transform.sca, &p_buffer[i]);
+
+    // Done
+    return i;
+}
